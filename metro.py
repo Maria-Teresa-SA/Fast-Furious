fbebe2c2d9b812e14b2085f98ba2633851f2bbb1
orlandoroldan <silvia.fabregas@estudiantat.upc.edu>
2022-05-08 12:56:12 +0200

    Canvis en metro.py

--- metro.py
+++ metro.py
@@ -5,19 +5,18 @@ from typing import Optional, TextIO, List
 from collections import namedtuple
 import networkx as nx
 
-# import matplotlib
-# matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
 #import staticmaps
-
 
 # falta typealias
 Position = namedtuple('Position', ['x', 'y'])
 
 
+
 @dataclass
-class Station:  # node del graf
+class Station:  
+    """Representa un node del graf. Estacions de metro."""
     name: TypeAlias = str
     line: TypeAlias = str
     order: TypeAlias = int
@@ -25,7 +24,8 @@ class Station:  # node del graf
 
 
 @dataclass
-class Access:  # node del graf
+class Access:
+    """Representa un node del graf. Accessos a les estacions de metro."""
     name_access: TypeAlias = str
     name_station: TypeAlias = str
     line: TypeAlias = str
@@ -33,6 +33,7 @@ class Access:  # node del graf
     position: TypeAlias = Position
 
 
+# encara NO definit
 @dataclass
 class Tram:  # aresta del graf
     line: TypeAlias = str
@@ -40,6 +41,7 @@ class Tram:  # aresta del graf
     color: TypeAlias = str
 
 
+# encara NO definit
 @dataclass
 class Enllac:  # aresta del graf
     distance: TypeAlias = float
@@ -54,65 +56,84 @@ Stations: TypeAlias = List[Station]
 
 Accesses: TypeAlias = List[Access]
 
-
 MetroGraph: TypeAlias = nx.Graph
-
-# FALTA CANVIAR-HO --> NOU MAIL SILVIA
 
 
 def read_stations() -> Stations:
+    """Llegeix un fitxer csv amb la informació requerida de les estacions de metro de Barcelona i en retorna una llista."""
+
     taula_dades = pandas.read_csv("estacions_linia.csv", usecols = ["NOM_ESTACIO", "NOM_LINIA", "ORDRE_ESTACIO", "GEOMETRY"], keep_default_na = False, dtype={"NOM_ESTACIO": str, "NOM_LINIA": str, "ORDRE_ESTACIO": int, "GEOMETRY": str})
     stations = []
+
     for i, row in taula_dades.iterrows():
         p = row["GEOMETRY"].strip('POINT( )').split() # type: List[int]
-        ubi = Position(p[0], p[1])
-        station = Station(row["NOM_ESTACIO"], row["NOM_LINIA"], row["ORDRE_ESTACIO"], ubi)
+        station = Station(row["NOM_ESTACIO"], row["NOM_LINIA"], row["ORDRE_ESTACIO"], Position(p[0], p[1]))
         stations.append(station)
         
     return stations
 
 
 def read_accesses() -> Accesses:
-    columnes_guardem = ['NOM_ACCES', 'NOM_ESTACIO',
-                        'NOM_LINIA', 'NOM_TIPUS_ACCESSIBILITAT', 'GEOMETRY']
-    taula_accessos = pandas.read_csv(
-        "accessos_estacio_linia.csv", usecols=columnes_guardem, keep_default_na=False)
-    ls = []
+    """Llegeix un fitxer csv amb la informació requerida dels accessos de metro de Barcelona i en retorna una llista."""
+
+    taula_accessos = pandas.read_csv("accessos_estacio_linia.csv", usecols = ['NOM_ACCES', 'NOM_ESTACIO', 'NOM_LINIA', 'NOM_TIPUS_ACCESSIBILITAT', 'GEOMETRY'], keep_default_na=False, dtype = {"NOM_ACCES": str, "NOM_ESTACIO": str, "NOM_LINIA": str, "NOM_TIPUS_ACCESSIBILITAT": str, "GEOMETRY": str})
+    accesses = []
+
     for i, row in taula_accessos.iterrows():
+        p = row['GEOMETRY'].strip('POINT ( )').split()  # type List[str]
+        access = Access(row['NOM_ACCES'], row['NOM_ESTACIO'], row['NOM_LINIA'], row['NOM_TIPUS_ACCESSIBILITAT'], Position(p[0], p[1]))
+        accesses.append(access)
 
-        ubi = row['GEOMETRY'].strip('POINT ( )').split()  # type List[str]
-
-        access = Access(row['NOM_ACCES'], row['NOM_ESTACIO'], row['NOM_LINIA'],
-                        row['NOM_TIPUS_ACCESSIBILITAT'], Position(ubi[0], ubi[1]))
-        ls.append(access)
-
-    return ls
+    return accesses
 
 
 def get_metro_graph() -> MetroGraph:
+    """Genera el graf amb nodes estacions i accessos als metros i amb les arestes requerides."""
+    
     G = nx.Graph() 
-    stations, accesos = read_stations(), read_accesses()
-    dictionary = {}
-    G.add_nodes_from([x for x in range(len(stations))])
-    for i in range(len(stations)):
-        dictionary[i] = stations[i]
 
-    # G.add_nodes_from(accesos)
+    # versió 1: fem servir un diccionari 
+    Dict = {} # type: Dict[str, List[int]]
+    # ESTACIONS:
+    stations = read_stations()
+    # nodes
+    for i in range(len(stations)):
+        s = stations[i] # els assignem un identificador i, ja que vàries estacions tenen el mateix nom tot i ser de línies diferents
+        G.add_node(i, name = s.name, line = s.line, order = s.order, position = s.position)
+        
+        if s.name in Dict.keys():
+            Dict[s.name].append(i)
+        else:
+            Dict[s.name] = [i]
+
+    # arestes
     llista = [(i-1, i) for i in range(1, len(stations)) if stations[i-1].line == stations[i].line]
     G.add_edges_from(llista)
+    
+    # ACCESSOS:
+    accesses = read_accesses()
+    # nodes
+    for a in accesses:
+        G.add_node(a.name_access, station_name = a.name_station, line = a.line, accessibility = a.accessibility, position = a.position)            
+        # arestes
+        for i in Dict[a.name_station]:
+            G.add_edge(i, a.name_access)
+
     return G
 
 
-
 def show(g: MetroGraph) -> None:
-    nx.draw(g)
+    """Mostra el graf amb els nodes de metros i accesos de la ciutat i les seves arestes."""
+    nx.draw(g, pos = nx.get_node_attributes(g, 'position'))
     plt.show()
 
-    # desa el graf com a imatge amb el mapa de la ciutat com a fons en l'arxiu especificat a filename
-    # usar staticmaps
 
 
-def plot(g: MetroGraph, filename: str) -> None: ...
+# desa el graf com a imatge amb el mapa de la ciutat com a fons en l'arxiu especificat a filename
+# usar staticmaps
+def plot(g: MetroGraph, filename: str) -> None:
+    m = StaticMap(width = 100000, height= 75000, url_template = filename)
+
 
 
 g = get_metro_graph()
