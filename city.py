8c833465a638c79eec8773ca63945c20471251c8
orlandoroldan <silvia.fabregas@estudiantat.upc.edu>
2022-05-12 16:31:04 +0200

    hem fet canvis crec

--- city.py
+++ city.py
@@ -1,75 +1,36 @@
-# Haurem de settejar els atributs de les edges
-
-
-# El primer cop que es demani el graf de carrers l'obtindrem a través d'osmnx i l'enmagatzemarem en un fitxer. Si es demana un altre cop pel graf, l'obtindrem llegint el fitxer a on estan les seves dades. 
-# funcions estan mig fetes ja
-
 from metro import *
 import osmnx as ox
 import os
-import pickle  # per guardar graf osmnx en un fitxer i no haver de crearlo cada cop
+import pickle                       # per guardar graf osmnx en un fitxer i no haver de baixar-lo cada cop. Serveix també per obrir el fitxer amb el graf quan ja ha set descarregat.
 
 CityGraph: TypeAlias = nx.Graph
 
 OsmnxGraph: TypeAlias = nx.MultiDiGraph
 
-fn = "barcelona.grf"
-
-# MÈTODE 1
 
 def get_osmnx_graph() -> OsmnxGraph:
-    return load_osmnx_graph(fn)
+    """Obté el graf dels carrers de Barcelona de la web mitjançant osmnx."""
+    return ox.graph_from_place('Barcelona', network_type='walk', simplify=True)
         
 def save_osmnx_graph(g: OsmnxGraph, filename: str) -> None:
-    """ens guarda el graf a un file"""
-    # guarda el graf g al fitxer filename
+    """Ens guarda el graf de carrers al fitxer de nom filename per poder-hi accedir futurament sense carregar-lo constantment de la web."""
 
-     # wb es igual a write in binary format
-    pickle_out = open(filname, 'wb')
+    pickle_out = open(filename, 'wb')
     pickle.dump(g, pickle_out)
     pickle_out.close()
 
 
 def load_osmnx_graph(filename: str) -> OsmnxGraph:
+    """Retorna el graf de carrers. En cas que és el primer cop que intentem accedir a aquest graf, 
+      se'l descarrega de la web, sinó accedeix al fitxer de nom filename on està guardat."""
 
     if not os.path.exists(filename):
-       # descargar graf dels carrers de barcelona
-        bcn_graph = ox.graph_from_place('Barcelona', network_type='walk', simplify=True)
+        bcn_streets = get_osmnx_graph()
         save_osmnx_graph(bcn_graph, filename)
-        return bcn_graph
-    # rb es igual a read in binary format
-    """ens retorna el graf guardat a filename"""
-    pickle_in = open(filname, "rb")
+        return bcn_streets
+    pickle_in = open(filename, "rb")
     bcn_streets = pickle.load(pickle_in)
     return bcn_streets
-    """ retorna el graf. si ja existia, el retorna, sinó, el loadeja i el retorna """
-    
-
-# MÈTODE 2
-
-# def get_osmnx_graph() -> OsmnxGraph:
-#     """Obté el graf dels carrers de Barcelona de la web mitjançant osmnx."""
-#     return ox.graph_from_place('Barcelona', network_type='walk', simplify=True)
-        
-# def save_osmnx_graph(g: OsmnxGraph, filename: str) -> None:
-#     """Ens guarda el graf de carrers al fitxer de nom filename per poder-hi accedir futurament sense carregar-lo constantment de la web."""
-
-#     pickle_out = open(filname, 'wb')
-#     pickle.dump(g, pickle_out)
-#     pickle_out.close()
-
-
-# def load_osmnx_graph(filename: str) -> OsmnxGraph:
-#     """Retorna el graf de carrers. En cas que és el primer cop que intentem accedir a aquest graf, 
-#       se'l descarrega de la web, sinó accedeix al fitxer de nom filename on està guardat."""
-
-#     if not os.path.exists(filename):
-#         bcn_streets = get_osmnx_graph()
-#         save_osmnx_graph(bcn_graph, filename)
-#         return bcn_streets
-#     pickle_in = open(filname, "rb")
-#     bcn_streets = pickle.load(pickle_in)
-#     return bcn_streets
    
 
 def build_city_graph(g1: OsmnxGraph, g2: MetroGraph) -> CityGraph: 
@@ -93,22 +54,45 @@ def build_city_graph(g1: OsmnxGraph, g2: MetroGraph) -> CityGraph:
             # afegim una aresta entre u i v i hi associem tota la informació d'aquesta i el seu tipus.
             city_graph.add_edge(u, v, tipus = "enllaç", info = eattr)
 
-    # FALTA AFEGIR LES ARESTES ENTRE ELS ACCESSOS A LES ESTACIONS I ELS CARRERS.
     city_graph = nx.union(g2, city_graph)
+    tipus = nx.get_node_attributes(g2, 'tipus') # diccionari amb els tipus
+    list_x,list_y, list_a = [], [], []
+    for i in g2.nodes:
+        if tipus[i] == "access":
+            coords = g2.nodes[i]["position"]
+            list_x.append(coords.x)
+            list_y.append(coords.y)
+            list_a.append(i)
+    
+    nearest = ox.distance.nearest_nodes(g1, list_x, list_y)
+    j = 0
+    for i in list_a:
+        coords = g2.nodes[i]["position"]
+        city_graph.add_edge(i, nearest[j], tipus = "acces")
+        j += 1
+        
     return city_graph
 
-     
+H = build_city_graph(load_osmnx_graph("barcelona.grf"),  get_metro_graph())
+print(H.nodes())     
+
     
 
-# #
-# #
-# #
-# # def find_path(ox_g: OsmnxGraph, g: CityGraph, src: Coord, dst: Coord) -> Path: ...
-# #
-# #
-# # NodeID: TypeAlias = Union[int, str]
-# # Path: TypeAlias = List[NodeID]
-# #
+NodeID: TypeAlias = int
+Path: TypeAlias = List[NodeID]                   # tots els nodes tenen identificadors enters
+
+def find_path(ox_g: OsmnxGraph, g: CityGraph, src: Coord, dst: Coord) -> Path:
+  """Retorna el camí més curt des de les coordenades de sortida fins les coordenades destí.
+  Utilitza l'OsmnxGraph per trobar l'ID del node més proper a aquestes coordenades i empra el
+  city graph per calcular el camí més curt que les connecta."""
+
+
+# alguna cosa falla aquí ahhh
+  nearest_src = ox.distance.nearest_nodes(ox_g, src.x, src.y)
+  nearest_dst = ox.distance.nearest_nodes(ox_g, dst.x, dst.y)
+  return nx.shortest_path(g, nearest_src, nearest_dst)
+
+
 # #
 # # def show(g: CityGraph) -> None: ...
 # # # mostra g de forma interactiva en una finestra
@@ -121,23 +105,6 @@ def build_city_graph(g1: OsmnxGraph, g2: MetroGraph) -> CityGraph:
 # def plot_path(g: CityGraph, p: Path, filename: str, ...) -> None: ...
 # # mostra el camí p en l'arxiu filename
 
-
-# def save_osmnx_graph(g: OsmnxGraph, filename: str) -> None: ... 
-#     # guarda el graf g al fitxer filename
-
-
-# def load_osmnx_graph(filename: str) -> OsmnxGraph: ... 
-
-#     if os.path.exists(filename):
-#         return blah blah
-
-#     # retorna el graf guardat al fitxer filename
-
-
-# # g2 és el graf calculat a get_metro_graph()
-# # s'han de fusionar aquest i el graf que es loadeja UNIÓ nodes STREET/STATION/ACCESS. UNIÓ arestes CARRER/ACCÉS/ENLLAÇ/TRAM com poden ser unhashable types nodes?? preguntar
-# # S'ha de connectar cada accés amb node més proper de tipus street. Arestes de tipus Street.
-# def build_city_graph(g1: OsmnxGraph, g2: MetroGraph) -> CityGraph
 
 
 
