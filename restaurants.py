0d69caf9363faf78711c9830f25b105de65af4d4
orlandoroldan <silvia.fabregas@estudiantat.upc.edu>
2022-05-28 18:27:17 +0200

    vale sí

--- restaurants.py
+++ restaurants.py
@@ -1,10 +1,11 @@
 # llibreries importades
 
-import pandas                                   # llegir fitxer csv
-from dataclasses import dataclass               # classes
-from typing import Optional, TextIO, List       # typing
-from collections import namedtuple              # generar tuples (Coord)
-from fuzzysearch import find_near_matches       # fer cerca difusa
+import pandas                                          # llegir fitxer csv
+from dataclasses import dataclass                      # classes
+from typing_extensions import TypeAlias                # typealiases
+from typing import Optional, TextIO, List, Tuple       # typing
+from collections import namedtuple                     # generar tuples (Coord)
+from fuzzysearch import find_near_matches              # fer cerca difusa
 
 categories = ['Restaurants', 'Tablaos flamencs', 'Cocteleries','Xampanyeries', 'Bars i pubs musicals' , 'Discoteques', 'Karaokes', 'Teatres']
 
@@ -12,22 +13,23 @@ categories = ['Restaurants', 'Tablaos flamencs', 'Cocteleries','Xampanyeries', '
 #   Tipus de dades   #
 ######################
 
-Coord = namedtuple('Coord', ['x', 'y']) # type = tuple(longitude, latitude)
+# (longitud, latitud)
+Coord = namedtuple('Coord', ['x', 'y']) # type = Tuple[float, float]
 
 @dataclass
 class Address:
-  address_name: str # nom del carrer
-  address_num: Optional[int] # número del carrer, falta tractar amb els valors Nan
-  neighbourhood: str # barri
-  district: str # districte
-  zip_code: int # codi postal
+  address_name: TypeAlias = str # nom del carrer
+  address_num: TypeAlias = Optional[int] # número del carrer, falta tractar amb els valors Nan
+  neighbourhood: TypeAlias = str # barri
+  district: TypeAlias = str # districte
+  zip_code: TypeAlias = int # codi postal
 
 @dataclass
 class Restaurant: 
-  name: str # nom del restaurant
-  address: Address # adressa del restaurant
-  category : str # tipus de restaurant
-  position: Coord # coordenades del restaurant
+  name: TypeAlias = str # nom del restaurant
+  address: TypeAlias = Address # adressa del restaurant
+  category : TypeAlias = str # tipus de restaurant
+  position: TypeAlias = Coord # coordenades del restaurant
 
 Restaurants = List[Restaurant]
 
@@ -46,13 +48,13 @@ def read_restaurants() -> Restaurants:
     """Llegeix el fitxer restaurants.csv i retorna una llista amb tots els restaurants de la ciutat de Barcelona amb les dades pertinents."""
 
     # columnes que guardem
-    c_g = ['name', 'addresses_road_name', 'addresses_start_street_number', 'addresses_neighborhood_name',
+    cols = ['name', 'addresses_road_name', 'addresses_start_street_number', 'addresses_neighborhood_name',
                         'addresses_district_name', 'addresses_zip_code', 'secondary_filters_name', 'geo_epgs_4326_x','geo_epgs_4326_y']
     # lectura de dades
-    taula_dades = pandas.read_csv("restaurants.csv", usecols=c_g, keep_default_na=False, dtype={c_g[0]: str, c_g[1]: str, c_g[2]: str, c_g[3]: str, c_g[4]: str, c_g[5]: str, c_g[6]: str, c_g[7]: float, c_g[8]: float})
+    data_table = pandas.read_csv("restaurants.csv", usecols=cols, keep_default_na=False, dtype={cols[0]: str, cols[1]: str, cols[2]: str, cols[3]: str, cols[4]: str, cols[5]: str, cols[6]: str, cols[7]: float, cols[8]: float})
                                                                                                    
     ls = []  # type: Restaurants
-    for i, row in taula_dades.iterrows():
+    for i, row in data_table.iterrows():
         restaurant = Restaurant(row['name'], Address(row['addresses_road_name'], row['addresses_start_street_number'],
                                 row['addresses_neighborhood_name'], row['addresses_district_name'], row['addresses_zip_code']),  row['secondary_filters_name'], Coord(row['geo_epgs_4326_y'], row['geo_epgs_4326_x']))
         ls.append(restaurant)
@@ -64,11 +66,11 @@ def read_restaurants() -> Restaurants:
 #     Cerca     #
 #################
 
-def myFunc(e):
+def myFunc(rest: Tuple[Restaurant, int]):
 
   """Criteri d'ordenació dels restaurants quan es realitza una búsqueda - De major a menor pes."""
 
-  return e[1]
+  return rest[1]
 
 def find_restaurants(queries: List[str], restaurants: Restaurants) -> Restaurants:
 
@@ -83,40 +85,43 @@ def find_restaurants(queries: List[str], restaurants: Restaurants) -> Restaurant
   for i in range(len(queries)):
     queries[i] = queries[i].title()   # millorar coincidències. Majoria de paraules es troben en aquest format al fitxer de restaurants.
     
-  pesos = [] # pesos és una llista de parelles de [Restaurant, pes_associat]
+  weigths = [] # weigths és una llista de parelles de [Restaurant, pes_associat]
 
   # només busquem dins les categories si hi ha un encert total (no fuzzysearch) per evitar problemes amb la fuzzysearch
   # i parelles de paraules com Sants i Restaurants.
-  buscar_categories = False
+  search_categories = False
   for query in queries:
     for category in categories:
-      buscar_categories = query in category
-      if buscar_categories: break
-    if buscar_categories: break
+      search_categories = query in category
+      if search_categories: break
+    if search_categories: break
       
 
   for restaurant in restaurants:
-    pes = 0        
+    w = 0 # pes inicial        
     for query in queries:
-      coincidencia = False      # si coincidència és True : restaurant coincideix amb la cerca. Pes és la seva prioritat.        
+      match = False      # si coincidència és True : restaurant coincideix amb la cerca. Pes és la seva prioritat.        
       for el in [restaurant.address.district, restaurant.address.neighbourhood, restaurant.address.address_name, restaurant.name]:
         fuzzy = find_near_matches(query, el, max_l_dist = 1)
         if len(fuzzy):
-          coincidencia = True
-          pes += 2 - fuzzy[0].dist
+          match = True
+          w += 2 - fuzzy[0].dist
           break
 
       # en cas que hem definit que volem buscar dins les categories, si encara no ha coincidit
-      if not coincidencia and buscar_categories and query in restaurant.category:
-        coincidencia = True
-        pes += 2
+      if not match and search_categories and query in restaurant.category:
+        match = True
+        w += 2
 
       # si una query no encaixa amb cap element del restaurant, no l'afegirem
-      if not coincidencia: break
+      if not match: break
 
-    if coincidencia: 
-      pesos.append([restaurant, pes])
+    if match: 
+      weigths.append([restaurant, w])
 
-  pesos.sort(reverse=True, key=myFunc)
+  weigths.sort(reverse=True, key=myFunc)
 
-  return [el[0] for el in pesos] 
+  return [el[0] for el in weigths] 
+
+
+
