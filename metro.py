0f66f0d04cb2501270f964652f88edb692d79ab5
Silf <103830755+orlandoroldan@users.noreply.github.com>
2022-06-03 16:04:48 +0200

    metro.py passat pel pep

--- metro.py
+++ metro.py
@@ -1,23 +1,25 @@
-import pandas as pd                                                   # llegir fitxers csv
-from dataclasses import dataclass                                     # dataclasses
-from typing_extensions import TypeAlias                               # typing
-from typing import Optional, TextIO, List                             # typing
-from collections import namedtuple                                    # generar tuples (Coord)
-import networkx as nx                                                 # generar graf
-import matplotlib.pyplot as plt                                       # plotejar mapa
-from staticmap import StaticMap, Line, CircleMarker                   # plotejar mapa
-from haversine import haversine, Unit                                 # calcular distàncies entre coordenades
+import pandas as pd                                         # llegir fitxers csv
+from dataclasses import dataclass                           # dataclasses
+from typing_extensions import TypeAlias                     # typing
+from typing import Optional, TextIO, List                   # typing
+from collections import namedtuple                          # generar tuples (Coord)
+import networkx as nx                                       # generar graf
+import matplotlib.pyplot as plt                             # plotejar mapa
+from staticmap import StaticMap, Line, CircleMarker         # plotejar mapa
+from haversine import haversine, Unit                       # calcular distàncies entre coordenades
 
 
 ######################
 #   Tipus de dades   #
 ######################
 
-Coord: TypeAlias = namedtuple('Coord', ['long', 'lat']) # (longitude, latitude)
+Coord: TypeAlias = namedtuple('Coord', ['long', 'lat'])
+
 
 @dataclass
 class Station:
-    """Infpormació associada a un node del graf de tipus Estacions de metro."""
+    """Infpormació associada a un node del graf de tipus Estacions
+    de metro."""
     name: TypeAlias = str               # nom de l'estació
     line: TypeAlias = str               # línia de mtero
     coord: TypeAlias = Coord            # coordenades de l'estació
@@ -26,7 +28,8 @@ class Station:
 
 @dataclass
 class Access:
-    """Informació associada a un node del graf de tipus Accessos a les estacions de metro."""
+    """Informació associada a un node del graf de tipus Accessos a
+    les estacions de metro."""
     name_access: TypeAlias = str        # nom de l'accés
     name_station: TypeAlias = str       # nom de l'estació associada a l'accés
     coord: TypeAlias = Coord            # coordenades de l'accés
@@ -51,15 +54,16 @@ MetroGraph: TypeAlias = nx.Graph
 
 # Pre: tenir el fitxer csv estacions_linia.csv
 def read_stations() -> Stations:
-    """Llegeix un fitxer csv amb la informació requerida de les estacions de metro de Barcelona i en retorna una llista d'aquestes."""
+    """Llegeix un fitxer csv amb la informació requerida de les estacions de metro de
+    Barcelona i en retorna una llista d'aquestes."""
 
-    cols = ["NOM_ESTACIO", "NOM_LINIA", "GEOMETRY", "COLOR_LINIA"] # columnes que guardem
-    data_table_stations = pd.read_csv("estacions_linia.csv", usecols = cols, keep_default_na=False, dtype={cols[0]: str, cols[1]: str, cols[2]: str, cols[3]: str})
-    
+    cols = ["NOM_ESTACIO", "NOM_LINIA", "GEOMETRY", "COLOR_LINIA"]  # columnes que guardem
+    data_table_stations = pd.read_csv("estacions_linia.csv", usecols=cols, keep_default_na=False, dtype={cols[0]: str, cols[1]: str, cols[2]: str, cols[3]: str})
+
     stations: Stations = []
     for i, row in data_table_stations.iterrows():
-        p: List[str] = row["GEOMETRY"].strip('POINT( )').split() 
-        s = Station(row["NOM_ESTACIO"], row["NOM_LINIA"], Coord(float(p[0]), float(p[1])), '#' + row["COLOR_LINIA"])
+        p: List[str] = row[cols[2]].strip('POINT( )').split()
+        s = Station(row[cols[0]], row[cols[1]], Coord(float(p[0]), float(p[1])), '#' + row[cols[3]])
         stations.append(s)
 
     return stations
@@ -67,15 +71,16 @@ def read_stations() -> Stations:
 
 # Pre: tenir el fitxer csv accessos_estacio_linia.csv
 def read_accesses() -> Accesses:
-    """Llegeix un fitxer csv amb la informació requerida dels accessos de metro de Barcelona i en retorna una llista d'aquests."""
+    """Llegeix un fitxer csv amb la informació requerida dels accessos de metro
+    de Barcelona i en retorna una llista d'aquests."""
 
-    cols = ['NOM_ACCES', 'NOM_ESTACIO', 'NOM_LINIA', 'GEOMETRY'] #columnes que guardem
+    cols = ['NOM_ACCES', 'NOM_ESTACIO', 'NOM_LINIA', 'GEOMETRY']  # columnes que guardem
     data_table_accesses = pd.read_csv("accessos_estacio_linia.csv", usecols=cols, keep_default_na=False, dtype={cols[0]: str, cols[1]: str, cols[2]: str, cols[3]: str})
-    
+
     accesses: Accesses = []
     for i, row in data_table_accesses.iterrows():
-        p: List[str] = row['GEOMETRY'].strip('POINT ( )').split()
-        a = Access(row['NOM_ACCES'], row['NOM_ESTACIO'], Coord(float(p[0]), float(p[1])), 'black')
+        p: List[str] = row[cols[3]].strip('POINT ( )').split()
+        a = Access(row[cols[0]], row[cols[1]], Coord(float(p[0]), float(p[1])), 'black')
         accesses.append(a)
 
     return accesses
@@ -85,10 +90,12 @@ def read_accesses() -> Accesses:
 #   Graf metro   #
 ##################
 
-# Pre: el tipus és "Street", "Tran", "Access" o "Transfer" i la distància està en metres
-def set_time(dtype: str, dist: float) -> float:
-    """Funció que retorna el temps que es triga en recórrer una distància (d'un graf) depenent del tipus d'aresta (Street, Tram, Enllaç, Accés).
-    
+# Pre: el tipus és "Street", "Tran", "Access" o "Transfer" i
+# la distància està en metres
+def _set_time(dtype: str, dist: float) -> float:
+    """Funció que retorna el temps que es triga en recórrer una distància (d'un
+    graf) depenent del tipus d'aresta (Street, Tram, Enllaç, Accés).
+
     Velocitat mitja caminant -> 5km/h = 83 m/min
     Velocitat mitja en metro -> 26 km/h = 433.3 m/min
     Accessos -> 2.5 min d'espera més 3km/h velocitat d'escales = 2.5 + 50m/min
@@ -102,18 +109,23 @@ def set_time(dtype: str, dist: float) -> float:
 
 
 def get_metro_graph() -> MetroGraph:
-    """Genera el graf amb nodes Estacions i Accessos als metros i les arestes requerides entre aquests.
-    A cada node li és assignat un identificador i uns atributs que n'especifiquen el tipus i tota la informació necessària (ex. posició).
-    Les arestes també tenen un atribut amb la informació del seu tipus, el temps que es triga en ser recorregudes i el seu color.
+
+    """Genera el graf amb nodes Estacions i Accessos als metros i les arestes requerides
+    entre aquests.
+    A cada node li és assignat un identificador i atributs que n'especifiquen
+    el tipus i tota la informació necessària (ex. posició).
+    Les arestes també tenen un atribut amb la informació del seu tipus, temps que
+    es triga en ser recorregudes i el seu color.
     """
 
     G = nx.Graph()
-    
+
     stations: Stations = read_stations()
     accesses: Accesses = read_accesses()
 
-    n, m = len(stations), len(accesses)    
-    # diccionari que ens connectarà amb un Enllaç els nodes estació que comparteixen parada però no línia.
+    n, m = len(stations), len(accesses)
+    # diccionari que ens connectarà amb un Enllaç els nodes
+    # estació que comparteixen parada però no línia.
     repeated_stations: Dict[str, List[int]] = {}
     j = 0
 
@@ -121,27 +133,29 @@ def get_metro_graph() -> MetroGraph:
         s: Station = stations[id]
 
         # afegir estacions de metro (NODES)
-        G.add_node(id, dtype = "Station", name = s.name, position = s.coord, color = s.color, line=s.line)
-        
+        G.add_node(id, dtype="Station", name=s.name, position=s.coord, color=s.color, line=s.line)
+
         # afegir enllaços de metro (ARESTES)
         if s.name in repeated_stations.keys():
             for s2 in repeated_stations[s.name]:
-                G.add_edge(id, s2, dtype = "Transfer", time = set_time("Transfer", haversine(s.coord, G.nodes[s2]["position"], unit=Unit.METERS)), color = "black")
+                G.add_edge(id, s2, dtype="Transfer", time=_set_time("Transfer", haversine(s.coord, G.nodes[s2]["position"], unit=Unit.METERS)), color="black")
             repeated_stations[s.name].append(id)
-        else: repeated_stations[s.name] = [id]
+        else:
+            repeated_stations[s.name] = [id]
 
         # afegir accessos a l'estació (NODES I ARESTES)
         while j < m:
             a: Access = accesses[j]
             if a.name_station == s.name:
-                G.add_node(j + n, dtype = "Access", name = (a.name_access, a.name_station), position = a.coord, color = a.color)
-                G.add_edge(j + n, id, dtype = "Access", time = set_time("Access", haversine(s.coord, a.coord, unit=Unit.METERS)), color = "black")
+                G.add_node(j + n, dtype="Access", name=(a.name_access, a.name_station), position=a.coord, color=a.color)
+                G.add_edge(j + n, id, dtype="Access", time=_set_time("Access", haversine(s.coord, a.coord, unit=Unit.METERS)), color="black")
                 j += 1
-            else: break    
-        
+            else:
+                break
+
         # afegir trams de metro
         if id < n and s.line == stations[id-1].line:
-            G.add_edge(id, id-1, dtype = "Tram", time = set_time("Tram", haversine(s.coord, stations[id-1].coord, unit=Unit.METERS)), color = s.color)
+            G.add_edge(id, id-1, dtype="Tram", time=_set_time("Tram", haversine(s.coord, stations[id-1].coord, unit=Unit.METERS)), color=s.color)
 
     return G
 
@@ -151,8 +165,9 @@ def get_metro_graph() -> MetroGraph:
 #################
 
 # pre: nodes de g tenen atribut color
-def get_node_colors(g: MetroGraph):
-    """Traspassa la informació guardada en un diccionari de colors a una llista de colors per pintar els nodes al mapa.
+def _get_node_colors(g: MetroGraph):
+    """Traspassa la informació guardada en un diccionari de colors a una
+    llista de colors per pintar els nodes al mapa.
     Aquest diccionari s'obté de l'atribut color guardat al graf g."""
 
     dict_colors: Dict[int, str] = nx.get_node_attributes(g, 'color')
@@ -160,25 +175,26 @@ def get_node_colors(g: MetroGraph):
 
 
 # pre: arestes de g tenen atribut color
-def get_edge_colors(g: MetroGraph):
-    """Traspassa la informació guardada en un diccionari de colors a una llista de colors per pintar les arestes al mapa.
+def _get_edge_colors(g: MetroGraph):
+    """Traspassa la informació guardada en un diccionari de colors a una
+    llista de colors per pintar les arestes al mapa.
     Aquest diccionari s'obté de l'atribut color guardat al graf g."""
-    
+
     dict_colors: Dict[int, str] = nx.get_edge_attributes(g, 'color')
     return [dict_colors[x] for x in dict_colors]
 
 
 def show(g: MetroGraph) -> None:
-    """Mostra el graf amb les estacions de metro i els accesos a aquestes com a nodes i les seves arestes corresponents."""
+    """Mostra el graf amb les estacions de metro i els accesos a
+    aquestes com a nodes i les seves arestes corresponents."""
 
-    nx.draw(g, pos=nx.get_node_attributes(g, 'position'), node_size=10, node_color=get_node_colors(g), width = 2, edge_color = get_edge_colors(g))
+    nx.draw(g, pos=nx.get_node_attributes(g, 'position'), node_size=10, node_color=_get_node_colors(g), width=2, edge_color=_get_edge_colors(g))
     plt.show()
 
 
-# desa el graf com a imatge amb el mapa de la ciutat com a fons en l'arxiu especificat a filename
-# usar staticmap
 def plot(g: MetroGraph, filename: str) -> None:
-    """Guarda al fitxer "filename" un plot del graf de metros amb la ciutat de Barcelona de fons. S'usa staticmap."""
+    """Guarda al fitxer "filename" un plot del graf de metros amb
+    la ciutat de Barcelona de fons. S'usa staticmap."""
 
     m = StaticMap(1200, 800, 10)
 
@@ -189,7 +205,7 @@ def plot(g: MetroGraph, filename: str) -> None:
         size: int = 10 if g.nodes[u]['dtype'] == "Station" else 7
         marker = CircleMarker(coordinate_u, color, size)
         m.add_marker(marker)
-    
+
     # ARESTES
     for v in g.edges:
         coordinate_v: Coord = g.nodes[v[0]]['position']
